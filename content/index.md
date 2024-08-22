6797cfae75bccba5d308a5e77b6d52df17b711d0
Etienne <contact@etiennelb.com>
2024-08-22 15:17:34 +0200

    Quartz sync: Aug 22, 2024, 3:17 PM

---
+++ content/index.md
@@ -0,0 +1,9 @@
+---
+title: Welcome to Quartz
+---
+
+This is a blank Quartz installation.
+
+This is good :) [[Untitled]]
+
+See the [documentation](https://quartz.jzhao.xyz) for how to get started.
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "etienne on security",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "fr-FR",
+    baseUrl: "etiennelb.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
