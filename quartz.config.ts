002e16687d8f73f990940c0a39c114f9d9765760
etiennelb <contact@etiennelb.com>
2024-08-23 01:53:55 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "etienne on security",
+    pageTitle: "etiennelb",
+    //pageTitle: "etiennelb on security",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
